7cb6723ea42e91a0f56e18874756584ee108c89f
stigsanek <stigsanek@yandex.ru>
2019-06-10 00:27:07 +0400

    Добавляет функцию генерации моков

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
 'use strict';
+
+var map = document.querySelector('.map');
+
+//  Функция генерации случайных данных
+var generateData = function () {
+  var items = [];
+  var offerType = ['palace', 'flat', 'house', 'bungalo'];
+  var getRandom = function (min, max) {
+    var random = Math.floor(min + Math.random() * (max - min));
+    return random;
+  };
+
+  for (var i = 0; i < 8; i++) {
+    items[i] = {
+      'author': {
+        'avatar': ''
+      },
+      'offer': {
+        'type': ''
+      },
+      'location': {
+        'x': '',
+        'y': ''
+      }
+    };
+    items[i]['author']['avatar'] = 'img/avatars/user0' + (i + 1) + '.png';
+    items[i]['offer']['type'] = offerType[Math.floor(Math.random() * offerType.length)];
+    items[i]['location']['x'] = Math.floor(Math.random() * map.clientWidth);
+    items[i]['location']['y'] = getRandom(130, 630);
+  }
+
+  return items;
+};
+
+generateData();
