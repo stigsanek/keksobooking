7217800c2aa2721e3cdc22b89a355fd296e99c1e
stigsanek <stigsanek@yandex.ru>
2019-06-15 20:22:08 +0400

    Изменяет названия переменных для хранения DOM-элементов в едином стиле

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@
 var PIN_WIDTH = 50;
 var PIN_HEIGHT = 70;
 
-var map = document.querySelector('.map');
-var mapPinsList = document.querySelector('.map__pins');
+var mapElement = document.querySelector('.map');
+var mapPinListElement = document.querySelector('.map__pins');
 
 //  Функция генерации случайных данных
 var generateData = function () {
@@ -32,58 +32,58 @@ var generateData = function () {
 
     items[i]['author']['avatar'] = 'img/avatars/user0' + (i + 1) + '.png';
     items[i]['offer']['type'] = offerType[Math.floor(Math.random() * offerType.length)];
-    items[i]['location']['x'] = Math.floor(Math.random() * mapPinsList.offsetWidth);
+    items[i]['location']['x'] = Math.floor(Math.random() * mapPinListElement.offsetWidth);
     items[i]['location']['y'] = getRandom(130, 630);
   }
 
   return items;
 };
 
-var newPin = document.querySelector('#pin').content.querySelector('.map__pin');
+var templatePinElement = document.querySelector('#pin').content.querySelector('.map__pin');
 
 //  Функция создания метки
-var createMapPin = function (objet) {
-  var mapPin = newPin.cloneNode(true);
-  var picture = mapPin.querySelector('img');
-  picture.src = objet['author']['avatar'];
-  picture.alt = 'Здесь будет заголовок объявления';
-  mapPin.style = 'left:' + (objet['location']['x'] - PIN_WIDTH / 2) + 'px; top:' + (objet['location']['y'] - PIN_HEIGHT) + 'px;';
+var createMapPin = function (objetPin) {
+  var newMapPinElement = templatePinElement.cloneNode(true);
+  var pictureElement = newMapPinElement.querySelector('img');
+  pictureElement.src = objetPin['author']['avatar'];
+  pictureElement.alt = 'Здесь будет заголовок объявления';
+  newMapPinElement.style = 'left:' + (objetPin['location']['x'] - PIN_WIDTH / 2) + 'px; top:' + (objetPin['location']['y'] - PIN_HEIGHT) + 'px;';
 
-  return mapPin;
+  return newMapPinElement;
 };
 
 //  Функция добавления меток в разметку
 var addMapPin = function (objetsPins) {
-  var fragment = document.createDocumentFragment();
+  var fragmentElement = document.createDocumentFragment();
 
   for (var i = 0; i < objetsPins.length; i++) {
-    var mapPinsElement = createMapPin(objetsPins[i]);
-    fragment.appendChild(mapPinsElement);
+    var mapPinElement = createMapPin(objetsPins[i]);
+    fragmentElement.appendChild(mapPinElement);
   }
 
-  mapPinsList.appendChild(fragment);
+  mapPinListElement.appendChild(fragmentElement);
 };
 
-var mapFormFilterFields = document.querySelector('.map__filters').querySelectorAll('select');
-var formFields = document.querySelectorAll('fieldset');
+var mapFormFilterElements = document.querySelector('.map__filters').querySelectorAll('select');
+var formFieldsElements = document.querySelectorAll('fieldset');
 
 // Функция добавления/удаления disabled всем полям форм
 var changeAttributeDisabled = function (isDisabled) {
   if (isDisabled) {
-    for (var i = 0; i < mapFormFilterFields.length; i++) {
-      mapFormFilterFields[i].disabled = true;
+    for (var i = 0; i < mapFormFilterElements.length; i++) {
+      mapFormFilterElements[i].disabled = true;
     }
-    for (i = 0; i < formFields.length; i++) {
-      formFields[i].disabled = true;
+    for (i = 0; i < formFieldsElements.length; i++) {
+      formFieldsElements[i].disabled = true;
     }
   }
 
   if (!isDisabled) {
-    for (i = 0; i < mapFormFilterFields.length; i++) {
-      mapFormFilterFields[i].disabled = false;
+    for (i = 0; i < mapFormFilterElements.length; i++) {
+      mapFormFilterElements[i].disabled = false;
     }
-    for (i = 0; i < formFields.length; i++) {
-      formFields[i].disabled = false;
+    for (i = 0; i < formFieldsElements.length; i++) {
+      formFieldsElements[i].disabled = false;
     }
   }
 };
@@ -92,21 +92,21 @@ var changeAttributeDisabled = function (isDisabled) {
 changeAttributeDisabled(true);
 
 // Активация страницы при клике на метку
-var mainForm = document.querySelector('.ad-form');
+var mainFormElement = document.querySelector('.ad-form');
 
 var activatePage = function () {
   // Включаем поля форм и убираем классы неактивного состояния
   changeAttributeDisabled(false);
-  map.classList.remove('map--faded');
-  mainForm.classList.remove('ad-form--disabled');
+  mapElement.classList.remove('map--faded');
+  mainFormElement.classList.remove('ad-form--disabled');
 
   // Генерируем массив данных и добавляем метки на карту
   var randomData = generateData();
   addMapPin(randomData);
 };
 
-var headPin = map.querySelector('.map__pin--main');
+var headPinElement = mapElement.querySelector('.map__pin--main');
 
-headPin.addEventListener('click', function () {
+headPinElement.addEventListener('click', function () {
   activatePage();
 });
