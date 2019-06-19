ebbc129b4ae2a2837247728f250c376c3573491e
stigsanek <stigsanek@yandex.ru>
2019-06-19 11:56:13 +0400

    Изменяет блок валидации формы

--- js/main.js
+++ js/main.js
@@ -135,14 +135,14 @@ headPinElement.addEventListener('mouseup', function () {
   getCoordinateHeadPin(HEAD_PIN_WIDTH, HEAD_PIN_HEIGHT);
 });
 
-// Валидация поля с ценой в зависимости от типа жилья
+// Валидация формы подачи объявления
+//// Определение минимальной стоимости в зависимости от типа выбранного жилья
 var priceInputElement = mainFormElement.querySelector('#price');
 var typeSelectElement = mainFormElement.querySelector('#type');
 
 var onTypeSelectChange = function (typeSelect, priceInput) {
   typeSelectElement.addEventListener('change', function () {
     if (typeSelectElement.value === typeSelect) {
-      priceInputElement.min = priceInput;
       priceInputElement.placeholder = priceInput;
     }
   });
@@ -152,7 +152,96 @@ for (var i = 0; i < TYPE_OG_HOUSING.length; i++) {
   onTypeSelectChange(TYPE_OG_HOUSING[i], MIN_PRICE[i]);
 }
 
-// Валидация полей заезда/выезда
+//// Функция отображения сообщение об ошибке заполнения полей
+var showError = function (field, messageError) {
+  var containerElement = document.createElement('p');
+  containerElement.textContent = messageError;
+  field.style = 'outline: 3px solid red';
+  field.parentElement.appendChild(containerElement);
+  field.addEventListener('focus', closeError);
+};
+
+//// Функция закрытия сообщения об ошибке
+var closeError = function (evt) {
+  evt.preventDefault();
+  valid = true;
+  evt.target.style = false;
+  var error = evt.target.parentElement.querySelector('p');
+  evt.target.parentElement.removeChild(error);
+  evt.addEventListener('blur', removeListener);
+};
+
+//// Функция удаления обработчиков
+var removeListener = function (evt) {
+  evt.preventDefault();
+  evt.target.removeEventListener('focus', closeError);
+  evt.target.removeEventListener('blur', removeListener);
+};
+
+//// Функция валидации полей форм
+var checkForm = function (checkFields) {
+  for (var i = 0; i < checkFields.length; i++) {
+    if (checkFields[i].name === 'title') {
+      if (checkFields[i].value.length === 0) {
+        valid = false;
+        showError(checkFields[i], 'Заполните текст заголовка');
+      }
+
+      if (checkFields[i].value.length < 30 && checkFields[i].value.length > 0) {
+        valid = false;
+        showError(checkFields[i], 'Текст заголовка должен содержать не менее 30 символов');
+      }
+
+      if (checkFields[i].value.length > 100) {
+        valid = false;
+        showError(checkFields[i], 'Текст заголовка должен содержать не более 100 символов');
+      }
+    }
+
+    if (checkFields[i].name === 'price') {
+
+      if (checkFields[i].value.length === 0) {
+        valid = false;
+        showError(checkFields[i], 'Заполните цену объявления');
+      }
+
+      if (checkFields[i].value > 1000000) {
+        valid = false;
+        showError(checkFields[i], 'Минимальная цена бунгало 1 000 000 рублей');
+      }
+
+      if (checkFields[i].placeholder === '1000' && checkFields[i].value < 1000) {
+        valid = false;
+        showError(checkFields[i], 'Минимальная цена квартиры 1 000 рублей');
+      }
+
+      if (checkFields[i].placeholder === '5000' && checkFields[i].value < 5000) {
+        valid = false;
+        showError(checkFields[i], 'Минимальная цена дома 5 000 рублей');
+      }
+
+      if (checkFields[i].placeholder === '10000' && checkFields[i].value < 10000) {
+        valid = false;
+        showError(checkFields[i], 'Минимальная цена дворца 10 000 рублей');
+      }
+    }
+  };
+
+  if (valid) {
+    mainFormElement.submit();
+  }
+};
+
+//// Устанавливаем обработчик событий на форму для валидации при попытке отправки данных пользователем
+var valid = true;
+var fieldInputElements = mainFormElement.querySelectorAll('input');
+
+mainFormElement.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+  checkForm(fieldInputElements);
+});
+
+//// Синхронизация полей заезда/выезда
 var timeinSelectElement = mainFormElement.querySelector('#timein');
 var timeoutSelectElement = mainFormElement.querySelector('#timeout');
 
