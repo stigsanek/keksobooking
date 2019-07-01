20624be5d80c8ced53496833a2443ab13b009f75
stigsanek <stigsanek@yandex.ru>
2019-07-01 13:00:49 +0400

    Изменяет валидацию формы. Проверка через HTML

--- js/form.js
+++ js/form.js
@@ -2,12 +2,12 @@
 
 // Модуль валидации формы подачи объявления
 (function () {
-  var TYPE_OG_HOUSING = ['bungalo', 'flat', 'house', 'palace'];
-  var MIN_PRICE = [0, 1000, 5000, 10000];
-  var TIME_CHECK = ['12:00', '13:00', '14:00'];
-  var MIN_LENGTH_TITLE = 30;
-  var MAX_LENGTH_TITLE = 100;
-  var MAX_PRICE = 1000000;
+  var typeHousePriceMap = {
+    'bungalo': '0',
+    'flat': '1000',
+    'house': '5000',
+    'palace': '10000'
+  };
 
   var mainFormElement = document.querySelector('.ad-form');
   var formFieldsElements = document.querySelectorAll('fieldset');
@@ -26,14 +26,6 @@
     for (var i = 0; i < formFieldsElements.length; i++) {
       formFieldsElements[i].disabled = false;
     }
-
-    // Изменение минимальной цены в зависимости от типа жилья
-    checkSelectPrice(TYPE_OG_HOUSING, MIN_PRICE);
-
-    // Синхронизация полей заезда/выезда
-    TIME_CHECK.forEach(function (element) {
-      onTimeSelectChange(element);
-    });
   };
 
   // Метод заполнения поле адреса
@@ -46,120 +38,26 @@
   var priceInputElement = mainFormElement.querySelector('#price');
   var typeSelectElement = mainFormElement.querySelector('#type');
 
-  var onTypeSelectChange = function (typeSelect, priceInput) {
-    typeSelectElement.addEventListener('change', function () {
-      if (typeSelectElement.value === typeSelect) {
-        priceInputElement.placeholder = priceInput;
-      }
-    });
+  var onTypeSelectChange = function () {
+    priceInputElement.min = typeHousePriceMap[typeSelectElement.value];
+    priceInputElement.placeholder = typeHousePriceMap[typeSelectElement.value];
   };
 
-  // Функция установки минимальной стоимости в placeholder
-  var checkSelectPrice = function (objectTypeHousing, objectPrice) {
-    for (var i = 0; i < objectTypeHousing.length; i++) {
-      onTypeSelectChange(objectTypeHousing[i], objectPrice[i]);
-    }
-  };
+  typeSelectElement.addEventListener('change', onTypeSelectChange);
 
   // Синхронизация полей заезда/выезда
   var timeinSelectElement = mainFormElement.querySelector('#timein');
   var timeoutSelectElement = mainFormElement.querySelector('#timeout');
 
-  var onTimeSelectChange = function (timeSelect) {
-    timeinSelectElement.addEventListener('change', function () {
-      if (timeinSelectElement.value === timeSelect) {
-        timeoutSelectElement.value = timeSelect;
-      }
-    });
-    timeoutSelectElement.addEventListener('change', function () {
-      if (timeoutSelectElement.value === timeSelect) {
-        timeinSelectElement.value = timeSelect;
-      }
-    });
+  var onTimeInChange = function () {
+    timeoutSelectElement.value = timeinSelectElement.value
+  };
+  var onTimeOutChange = function () {
+    timeinSelectElement.value = timeoutSelectElement.value
   };
 
-  // Функция отображения сообщения об ошибке заполнения полей
-  var showError = function (field, messageError) {
-    var containerElement = document.createElement('p');
-    containerElement.classList.add('error-valid');
-    containerElement.textContent = messageError;
-    field.style = 'outline: 3px solid #ff5635';
-    field.parentElement.appendChild(containerElement);
-    field.addEventListener('focus', closeError);
-  };
-
-  // Функция закрытия сообщения об ошибке
-  var closeError = function (evt) {
-    evt.preventDefault();
-    valid = true;
-    evt.target.style = false;
-    var error = evt.target.parentElement.querySelector('p');
-    evt.target.parentElement.removeChild(error);
-    evt.addEventListener('blur', removeListener);
-  };
-
-  // Функция удаления обработчиков
-  var removeListener = function (evt) {
-    evt.preventDefault();
-    evt.target.removeEventListener('focus', closeError);
-    evt.target.removeEventListener('blur', removeListener);
-  };
-
-  // Функция валидации полей форм
-  var checkForm = function (checkFields) {
-    for (var i = 0; i < checkFields.length; i++) {
-      // Проверяем заголовок объявления
-      if (checkFields[i].name === 'title') {
-        if (checkFields[i].value.length === 0) {
-          valid = false;
-          showError(checkFields[i], 'Заполните текст заголовка');
-        }
-
-        if (checkFields[i].value.length < MIN_LENGTH_TITLE && checkFields[i].value.length > 0) {
-          valid = false;
-          showError(checkFields[i], 'Текст заголовка должен содержать не менее ' + MIN_LENGTH_TITLE + ' символов');
-        }
-
-        if (checkFields[i].value.length > MAX_LENGTH_TITLE) {
-          valid = false;
-          showError(checkFields[i], 'Текст заголовка должен содержать не более ' + MAX_LENGTH_TITLE + ' символов');
-        }
-      }
-      // Проверяем цену в зависимости от типа жилья
-      var placeholderValue = parseInt(checkFields[i].getAttribute('placeholder'), 10);
-
-      if (checkFields[i].name === 'price') {
-
-        if (checkFields[i].value.length === 0 || checkFields[i].value === '0') {
-          valid = false;
-          showError(checkFields[i], 'Заполните цену объявления');
-        }
-
-        if (checkFields[i].value > MAX_PRICE) {
-          valid = false;
-          showError(checkFields[i], 'Максимальная цена ' + MAX_PRICE);
-        }
-
-        if (checkFields[i].value > 0 && checkFields[i].value < placeholderValue) {
-          valid = false;
-          showError(checkFields[i], 'Минимальная цена ' + placeholderValue);
-        }
-      }
-    }
-    // Отправляем форму
-    if (valid) {
-      mainFormElement.submit();
-    }
-  };
-
-  // Устанавливаем обработчик валидации на форму
-  var valid = true;
-  var fieldInputElements = mainFormElement.querySelectorAll('input');
-
-  mainFormElement.addEventListener('submit', function (evt) {
-    evt.preventDefault();
-    checkForm(fieldInputElements);
-  });
+  timeinSelectElement.addEventListener('change', onTimeInChange);
+  timeoutSelectElement.addEventListener('change', onTimeOutChange);
 
   window.form = {
     disable: disableForm,
